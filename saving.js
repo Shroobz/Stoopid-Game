09737679807e0c64d4c87fd6ef3297bf771cef5f
oijhhiooooo <98014200@jrl.nl>
2024-11-01 14:07:53 +0100

    save patch by force

--- saving.js
+++ saving.js
@@ -51,8 +51,7 @@ function load() {
     if (typeof savegame.up2 !== "undefined") up2 = new Decimal(savegame.up2);
     if (typeof savegame.up3 !== "undefined") up3 = new Decimal(savegame.up3);
     if (typeof savegame.up4 !== "undefined") up4 = new Decimal(savegame.up4);
-    if (typeof savegame.pup1 !== "undefined") pup1 = savegame.pup1;
-    if (pup1 != true && pup1 != false) {pup1 = false;}
+    if (typeof savegame.pup1 !== "undefined" && savegame.savetime > 1730466458683) pup1 = savegame.pup1;
     console.log("done loading")
     console.log("calculating offline progress")
     if (typeof savegame.savetime !== "undefined") {
